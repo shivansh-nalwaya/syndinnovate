1d0b2ebbe76fa0ea74cc7148914155b634910aa0
Shivansh Nalwaya <shivanshnalwaya@gmail.com>
2019-08-27 17:14:57 +0530

    Add drawer in header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,32 +6,177 @@ import {
   Left,
   Right,
   Title,
-  View
+  View,
+  ListItem,
+  Text,
+  Picker,
+  Container,
+  List
 } from "native-base";
 import React, { Component } from "react";
-import { StyleSheet } from "react-native";
-import ActionModel from "../models/ActionModel";
+import { StyleSheet, TouchableHighlight, Dimensions } from "react-native";
+import Actions from "../models/ActionModel";
 import { observer } from "mobx-react";
+import styled from "styled-components";
+import posed from "react-native-pose";
+
+const Box = posed.View({
+  visible: {
+    left: 0,
+    transition: { duration: 300 }
+  },
+  hidden: {
+    left: -600,
+    transition: { duration: 300 }
+  }
+});
+
+const StyledBox = styled(Box)`
+  position: absolute;
+  left: -100;
+  width: 300;
+  z-index: 5;
+  height: ${Dimensions.get("window").height};
+`;
+
+const LightContainer = styled(Container)``;
+
+const PaddedList = styled(List)`
+  margin-right: 20px;
+`;
+
+const routes = [
+  { name: "Home", action: "dashboard", icon: "home" },
+  { name: "Enquiries", action: "leads", icon: "address-book" },
+  { name: "Admissions", icon: "graduation-cap" },
+  { name: "Batches", action: "downloaded_batch_page", icon: "users" },
+  { name: "Clock In / Clock Out", action: "user_attendance", icon: "clock-o" },
+  {
+    name: "Logout",
+    icon: "sign-out",
+    onPress: () => {
+      Sessions.logout();
+    }
+  }
+];
 
 class CustomHeader extends Component {
+  state = { isOpen: false };
+
   render() {
-    if (ActionModel.hideHeaderFooter) return <View />;
+    if (Actions.hideHeaderFooter) return <View />;
     return (
-      <Header style={styles.header}>
-        <Left>
-          <Button transparent>
-            <Icon name="menu" />
-          </Button>
-        </Left>
-        <Body>
-          <Title>{this.props.title || "Home"}</Title>
-        </Body>
-        <Right>
-          <Button transparent>
-            <Icon name="md-notifications" />
-          </Button>
-        </Right>
-      </Header>
+      <React.Fragment>
+        <Header style={styles.header}>
+          <Left>
+            <Button
+              transparent
+              onPress={() => {
+                this.setState({ isOpen: !this.state.isOpen });
+              }}
+            >
+              <Icon name="menu" />
+            </Button>
+          </Left>
+          <Body>
+            <Title>{this.props.title || "Home"}</Title>
+          </Body>
+          <Right>
+            <Button transparent>
+              <Icon name="md-notifications" />
+            </Button>
+          </Right>
+        </Header>
+        <View style={{ width: "100%" }}>
+          <StyledBox pose={this.state.isOpen ? "visible" : "hidden"}>
+            <LightContainer style={{ elevation: 50 }}>
+              <View style={{ margin: 20 }}>
+                <Text>Current Center</Text>
+                <View
+                  style={{
+                    borderWidth: 1,
+                    borderColor: "#9a9a9a",
+                    borderRadius: 5,
+                    marginTop: 4
+                  }}
+                >
+                  <Picker
+                    onValueChange={id => {
+                      this.setState({ isOpen: false });
+                      Sessions.setCenter(id)
+                        .then(() => {
+                          Toast.show({
+                            text: "Center changed",
+                            duration: 2000
+                          });
+                          Actions.jump("dashboard");
+                        })
+                        .catch(() => {
+                          Toast.show({
+                            text: "Unable to change center",
+                            duration: 2000
+                          });
+                        });
+                    }}
+                  >
+                    {[{ label: "1", value: "1" }].map(c => {
+                      return (
+                        <Picker.Item
+                          key={c.value}
+                          label={c.label}
+                          value={c.value}
+                        />
+                      );
+                    })}
+                  </Picker>
+                </View>
+              </View>
+              <PaddedList>
+                {routes.map((data, ind) => {
+                  return (
+                    <ListItem
+                      icon
+                      key={ind}
+                      style={{ marginVertical: 5 }}
+                      onPress={() => {
+                        if (data.action) Actions.jump(data.action);
+                        else if (data.onPress) data.onPress();
+                      }}
+                    >
+                      <Left>
+                        <Icon
+                          type="FontAwesome"
+                          name={data.icon}
+                          style={{ fontSize: 20 }}
+                        />
+                      </Left>
+                      <Body>
+                        <Text>{data.name}</Text>
+                      </Body>
+                    </ListItem>
+                  );
+                })}
+              </PaddedList>
+            </LightContainer>
+          </StyledBox>
+          {this.state.isOpen && (
+            <TouchableHighlight
+              style={{
+                backgroundColor: "rgba(0,0,0,0.5)",
+                position: "absolute",
+                zIndex: 4,
+                width: Dimensions.get("window").width,
+                height: Dimensions.get("window").height
+              }}
+              onPress={() => {
+                this.setState({ isOpen: false });
+              }}
+            >
+              <Text />
+            </TouchableHighlight>
+          )}
+        </View>
+      </React.Fragment>
     );
   }
 }
